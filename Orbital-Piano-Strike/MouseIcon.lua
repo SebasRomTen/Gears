52903a0c5e81ddd03323c499e8219aa93059140d
SebasRomTen <85968889+SebasRomTen@users.noreply.github.com>
2023-04-02 16:25:02 -0600

    Create MouseIcon.lua

---
+++ Orbital-Piano-Strike/MouseIcon.lua
@@ -0,0 +1,24 @@
+local MOUSE_ICON = 'rbxasset://textures/GunCursor.png'
+local RELOADING_ICON = 'rbxasset://textures/GunWaitCursor.png'
+
+local Tool = script.Parent
+
+local Mouse = nil
+
+local function UpdateIcon()
+	Mouse.Icon = Tool.Enabled and MOUSE_ICON or RELOADING_ICON
+end
+
+local function OnEquipped(mouse)
+	Mouse = mouse
+	UpdateIcon()
+end
+
+local function OnChanged(property)
+	if property == 'Enabled' then
+		UpdateIcon()
+	end
+end
+
+Tool.Equipped:connect(OnEquipped)
+Tool.Changed:connect(OnChanged)
