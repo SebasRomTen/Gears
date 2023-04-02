9721fa956e5fabd9a2d30ff83be3540b0e93e3d9
SebasRomTen <85968889+SebasRomTen@users.noreply.github.com>
2023-04-02 12:34:38 -0600

    Update LocalGui.lua

--- SubspaceTripmine/LocalGui.lua
+++ SubspaceTripmine/LocalGui.lua
@@ -1,31 +1,30 @@
+script.Name = "LocalGui"
 local Tool = script.Parent;
-
-enabled = true
-function onButton1Down(mouse)
-	if not enabled then
-		return
+	
+	local enabled = true
+	local function onButton1Down(mouse)
+		if not enabled then
+			return
+		end
+	
+		enabled = false
+		mouse.Icon = "rbxasset://textures\\ArrowFarCursor.png"
+	
+		wait(3)
+		mouse.Icon = "rbxasset://textures\\ArrowCursor.png"
+		enabled = true
+	
 	end
-
-	enabled = false
-	mouse.Icon = "rbxasset://textures\\ArrowFarCursor.png"
-
-	wait(3)
-	mouse.Icon = "rbxasset://textures\\ArrowCursor.png"
-	enabled = true
-
-end
-
-function onEquippedLocal(mouse)
-
-	if mouse == nil then
-		print("Mouse not found")
-		return 
+	
+	local function onEquippedLocal(mouse)
+	
+		if mouse == nil then
+			print("Mouse not found")
+			return 
+		end
+	
+		mouse.Icon = "rbxasset://textures\\ArrowCursor.png"
+		mouse.Button1Down:connect(function() onButton1Down(mouse) end)
 	end
-
-	mouse.Icon = "rbxasset://textures\\ArrowCursor.png"
-	mouse.Button1Down:connect(function() onButton1Down(mouse) end)
-end
-
-Tool.Equipped:connect(onEquippedLocal)
-
-script.Name = "Local Gui"
+	
+	Tool.Equipped:connect(onEquippedLocal)
