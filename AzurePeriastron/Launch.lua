510550e46e29a32db920061b2f8e244edaa9a710
SebasRomTen <85968889+SebasRomTen@users.noreply.github.com>
2023-04-03 15:55:57 -0600

    Update Launch.lua

--- AzurePeriastron/Launch.lua
+++ AzurePeriastron/Launch.lua
@@ -2,5 +2,7 @@
 	for i=1,5,1 do
 		Camera.CFrame = CFrame.new(i,i,i)
 	end]]
+local Dir = Instance.new("Vector3Value", script)
+Dir.Name = "Direction"
     wait(.9)
     script.Parent.Velocity = script:WaitForChild("Direction").Value
