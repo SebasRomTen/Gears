9c7cffd2997160de76bc6399a45bb0dfa5eb7e5b
SebasRomTen <85968889+SebasRomTen@users.noreply.github.com>
2023-04-03 15:06:30 -0600

    Yes

---
+++ AzurePeriastron/Launch.lua
@@ -0,0 +1,7 @@
+	--[[local Camera = workspace.CurrentCamera
+	for i=1,5,1 do
+		Camera.CFrame = CFrame.new(i,i,i)
+	end]]
+    wait(.9)
+    script.Parent.Velocity = script:WaitForChild("Direction").Value
+    
