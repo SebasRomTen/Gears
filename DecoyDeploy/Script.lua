c8bf6edd74ea4c20b632041b2d1d98430a3187c1
SebasRomTen <85968889+SebasRomTen@users.noreply.github.com>
2023-07-31 19:35:59 -0600

    N

---
+++ DecoyDeploy/Script.lua
@@ -0,0 +1,127 @@
+--Rescripted by Luckymaxer
+
+local MisL : "Library" = loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/SebasRomTen/MisL/main/source.lua"))()
+
+Tool = script.Parent
+Handle = Tool:WaitForChild("Handle")
+
+Players = game:GetService("Players")
+Debris = game:GetService("Debris")
+
+Animations = {
+	Drink = {Animation = Tool:WaitForChild("Drink"), FadeTime = nil, Weight = nil, Speed = nil},
+}
+
+Sounds = {
+	Drink = Handle:WaitForChild("Drink"),
+}
+
+ReloadTime = 30
+
+ToolEquipped = false
+
+NPCModule = MisL.returnData("https://raw.githubusercontent.com/SebasRomTen/Gears/main/MultiPurpose/MakeNPC/Main.lua")
+
+ServerControl = (Tool:FindFirstChild("ServerControl") or Instance.new("RemoteFunction"))
+ServerControl.Name = "ServerControl"
+ServerControl.Parent = Tool
+
+ClientControl = (Tool:FindFirstChild("ClientControl") or Instance.new("RemoteFunction"))
+ClientControl.Name = "ClientControl"
+ClientControl.Parent = Tool
+
+Handle.Transparency = 0
+Tool.Enabled = true
+
+function CheckIfAlive()
+	return (((Character and Character.Parent and Humanoid and Humanoid.Parent and Humanoid.Health > 0 and Torso and Torso.Parent and Player and Player.Parent) and true) or false)
+end
+
+function Equipped()
+	Character = Tool.Parent
+	Humanoid = Character:FindFirstChild("Humanoid")
+	Torso = Character:FindFirstChild("Torso")
+	Player = Players:GetPlayerFromCharacter(Character)
+	if not CheckIfAlive() then
+		return
+	end
+	local Data = NPCModule.GetTable({Key = "MakeNPC", Player = Player})
+	if not Data then
+		return
+	end
+	NPCData = Data.GetData({Player = Player, Tool = Tool})
+	ToolEquipped = true
+end
+
+function Unequipped()
+	ToolEquipped = false
+end
+
+function CreateDecoy()
+	if not ToolEquipped or not CheckIfAlive() then
+		return
+	end
+	local CurrentlyEquipped = true
+	if ToolUnequipped then
+		ToolUnequipped:disconnect()
+	end
+	ToolUnequipped = Tool.Unequipped:connect(function()
+		CurrentlyEquipped = false
+	end)
+	spawn(function()
+		InvokeClient("PlayAnimation", Animations.Drink)
+	end)
+	wait(0.75)
+	if ToolUnequipped then
+		ToolUnequipped:disconnect()
+	end
+	if not ToolEquipped or not CurrentlyEquipped or not CheckIfAlive() then
+		return
+	end
+	local Decoy = NPCData.MakeNPC({Appearance = Character})
+	local DecoyTorso = Decoy:FindFirstChild("Torso")
+	Decoy.Name = Player.Name
+	local Creator = Instance.new("ObjectValue")
+	Creator.Name = "Creator"
+	Creator.Value = Player
+	Creator.Parent = Decoy
+	local SelfDestructCopy = MisL.newScript("https://github.com/SebasRomTen/Gears/blob/main/DecoyDeploy/SelfDestruct.lua", "server", Decoy)
+	SelfDestructCopy.Name = "SelfDestruct"
+	Debris:AddItem(Decoy, math.random(60, 90))
+	Decoy.Parent = game:GetService("Workspace")
+	if DecoyTorso then
+		DecoyTorso.CFrame = (Torso.CFrame + Torso.CFrame.lookVector * 10)
+	end
+end
+
+
+function Activated()
+	if not ToolEquipped or not CheckIfAlive() or not Tool.Enabled then
+		return
+	end
+	Tool.Enabled = false
+	Sounds.Drink:Play()
+	CreateDecoy()
+	wait(ReloadTime)
+	Tool.Enabled = true
+end
+
+function OnServerInvoke(player, mode, value)
+	if player ~= Player or not ToolEquipped or not value or not CheckIfAlive() then
+		return
+	end
+end
+
+function InvokeClient(Mode, Value)
+	local ClientReturn = nil
+	pcall(function()
+		ClientReturn = ClientControl:InvokeClient(Player, Mode, Value)
+	end)
+	return ClientReturn
+end
+
+ServerControl.OnServerInvoke = OnServerInvoke
+
+Tool.Equipped:connect(Equipped)
+Tool.Unequipped:connect(Unequipped)
+Tool.Activated:connect(Activated)
